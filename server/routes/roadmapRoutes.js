7ac7369eadd39626b7872f370c0687e25a0a04df
Your Name <you@example.com>
2026-01-30 15:07:50 +0530

    fix: add missing generatePhaseQuiz import in roadmapRoutes

--- server/routes/roadmapRoutes.js
+++ server/routes/roadmapRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { generateRoadmap, getRoadmaps, updateProgress, validatePhaseQuiz } = require('../controllers/roadmapController');
+const { generateRoadmap, getRoadmaps, updateProgress, validatePhaseQuiz, generatePhaseQuiz } = require('../controllers/roadmapController');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
